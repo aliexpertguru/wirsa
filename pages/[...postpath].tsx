f4fa028ac85ff214e2851f83f5d9c1ac5f4f764c
aliexpertguru <66714871+aliexpertguru@users.noreply.github.com>
2023-06-07 02:46:59 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://womerr.com/graphql"
+	const endpoint = "https://wirsa.pk/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://womerr.com/` + encodeURI(path as string)
+					`https://wirsa.pk/` + encodeURI(path as string)
 				}`,
 			},
 		};
